c9881344bef8cc6f111e784193eb67b963b04741
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-05-24 15:11:47 +0000

    added the script

--- 8-square.js
+++ 8-square.js
@@ -1,1 +1,11 @@
-8-square.js
+const args = process.argv.slice(2);
+const size = parseInt(args[0], 10);
+
+if (isNaN(size)) {
+    console.log("Missing size");
+} else {
+    for (let i = 0; i < size; i++) {
+        console.log("X".repeat(size));
+    }
+}
+
