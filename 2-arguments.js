ea5c48c21b4338e85beaca4ae9837ccb351ad517
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-05-24 13:49:03 +0000

    corrected the code

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,10 @@
-//Write a script that prints a message depending of the number of arguments passed:
+const args = process.argv.slice(2);
 
-//If no arguments are passed to the script, print “No argument”
-//If only one argument is passed to the script, print “Argument found”
-//Otherwise, print “Arguments found”
-//You must use console.log(...) to print all output
-//You are not allowed to use var
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
 
-if()
